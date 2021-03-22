23e300671b9462f54e7c4dc26512a429bf49b960
Hyung-Taik Choi <htc.refactor@gmail.com>
2021-03-23 01:52:57 +0900

    Load other people's video on screen

--- public/script.js
+++ public/script.js
@@ -8,19 +8,33 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
+const peers = {}
+
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+socket.on('user-disconnected', userId => {
+    if(peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
-})
-
-socket.on('user-connected', userId => {
-    console.log('User connected: ' + userId)
 })
 
 function addVideoStream(video, stream) {
@@ -29,4 +43,17 @@ function addVideoStream(video, stream) {
         video.play()
     })
     videoGrid.append(video)
+}
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
 }
